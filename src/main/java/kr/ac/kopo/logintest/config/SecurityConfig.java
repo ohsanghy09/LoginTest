4e6c0a33d3403ee8017c29d60564a73b6adb4efd
ohsanghy09 <ohsanghy09@naver.com>
2024-11-12 16:27:10 +0900

    10.2 스프링 시큐리티 커스터마이징(username, password 설정해서 sign in)

--- src/main/java/kr/ac/kopo/logintest/config/SecurityConfig.java
+++ src/main/java/kr/ac/kopo/logintest/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package kr.ac.kopo.logintest.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
@@ -30,6 +32,20 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests((auth) -> {
+            //sample/all은 아무나 다 접근할 수 있도록 하기 위함
+            auth.requestMatchers("/sample/all").permitAll();
+        });
+
+        // 이거 에러 아님
+        httpSecurity.formLogin();
+
+        return httpSecurity.build();
+
     }
 
 }
